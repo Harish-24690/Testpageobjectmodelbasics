2c17bd47e549ae474b42c5cedfb0cd3ae875f70d
harish <harishr0691@gmail.com>
2021-06-29 15:40:00 +0530

    second commit

--- src/test/java/test/rough/LoginTest.java
+++ src/test/java/test/rough/LoginTest.java
@@ -15,12 +15,14 @@ import test.pages.ZohoApp;
 
 public class LoginTest {
 	
+	// Rough page from logintest
 	public static void main(String[] args) {
 		
 		System.out.println(System.getProperty("user.dir"));
 		HomePage home = new HomePage();
 		
 		LoginPage login =home.goToSignIn();     //page.goToFreeSignUp();
+		
 		
 		//LoginPage login = new LoginPage();
 		ZohoApp zapp=	login.doLogin("harishr24690@gmail.com" ,"harish9731332306");
